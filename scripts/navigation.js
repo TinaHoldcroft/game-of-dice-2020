02b263dae67c73abd7b2747749af9b26b9485831
TinaHoldcroft <54176460+TinaHoldcroft@users.noreply.github.com>
2020-07-27 14:54:49 +0200

    html nav

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,5 +1,5 @@
 //side panel toggle
-$('.navToggle').click(function(toggle) {
-    $(".side-panel-container").toggleClass("open"); //add open class when clicked
+$('#navToggle').click(function(toggle) {
+    $(".panel-container").toggleClass("open"); //add open class when clicked
     toogle.preventDefault();
 }
