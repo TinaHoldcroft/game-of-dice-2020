3409398b25ad97afd2d3cbdea99fccc5b442d734
TinaHoldcroft <54176460+TinaHoldcroft@users.noreply.github.com>
2020-07-28 11:51:01 +0200

    changed to if/else

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,13 +1,22 @@
 //sidepanel toggle
-$('.unicode').click(function(toogle) {
-    toogle.preventDefault();
-    $(".panel-container").toggleClass("openPanel"); //add class
-    console.log ('class added to panel-container')
+$('.unicode').click(function() {
+    if($('.panel-container').hasClass("openPanel")) { //close
+        $('.panel-container').removeClass('openPanel');
+        console.log ('Toggle closed: class removed')
+    }
+    else { //open          
+        $('.panel-container').addClass('openPanel');   
+        console.log ('Toggle opened: class added to panel-container')           
+    }
 });
-
 //dropdown menu
-$('#dropdown').click(function(dropdown) {
-    dropdown.preventDefault();
-    $("nav").toggleClass("showDropdown"); //add class
-    console.log ('class added to nav')
+$('#dropdown').click(function() { 
+    if($('nav').hasClass("showDropdown")) { //close
+        $('nav').removeClass('showDropdown');
+        console.log ('Dropdown closed: class removed')
+    }
+    else { //open          
+        $('nav').addClass('showDropdown');   
+        console.log ('Dropdown opened: class added to nav')           
+    }
 });
