87df7e4092d9cf1546807cb2861ec68ecc2c68eb
TinaHoldcroft <54176460+TinaHoldcroft@users.noreply.github.com>
2020-07-29 19:51:46 +0200

    js game and cards

--- scripts/game.js
+++ scripts/game.js
@@ -1,2 +1,10 @@
-//traps
+
 // https://github.com/joakimskoog/AnApiOfIceAndFire/blob/master/data/characters.json
+// add sound?
+// css modal?
+
+//You will need to create dice which can be rolled, and will only produce a number between 1 and 6
+//Whatever number the user rolls on the dice, their token will advance the number shown on the dice. 
+//From the start to the end there should be 30 tiles for the tokens to move on.
+//minimun 5 traps
+//When a six is rolled on the dice the system should know that the same player will get another turn. 
---
+++ scripts/winner.js
@@ -0,0 +1,1 @@
+//A bead or token will eventually need to reach the end of the game. When the token wins the game, you are going to have a finale page where you will congratulate the user that has won. This page needs to be aesthetically pleasing. It would be nice if you could do some JavaScript animations on this page to make it interactive. Use your imagination here.
